9cbc035fe3a28ec52e926e7018f7477f14e6bb6f
mali84211 <mali84211@gmail.com>
2024-07-11 23:28:38 +0300

    V1

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region V1
+
+            //Employee employee = new Employee(10,"ali",2233);
+            //Console.WriteLine(employee);
+            //id = 10;
+            //name = "ali";
+            //salary = 2233;
+
+
+            //employee.id = 20;   //set id directly through attribute
+            //Console.WriteLine(employee.id);    //get id directly through attribute    
+
+            //employee.name = "mona";
+            //Console.WriteLine(employee.name);
+
+            //employee.SetName("mona");
+            //Console.WriteLine(employee.GetName());
+
+            //employee.salary = 1000;
+            //Console.WriteLine(employee.salary);
+
+            //employee.salary = 2000;
+            //Console.WriteLine(employee.salary);
+
+            //employee.age = 30;
+
+            #endregion
+        }
+    }
+}
