03eea85fdb1ff0d0418fc5a5580f414f281f7044
mali84211 <mali84211@gmail.com>
2024-07-11 20:03:30 +0300

    Add project files.

---
+++ C42-G02-OOP02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C42_G02_OOP02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
