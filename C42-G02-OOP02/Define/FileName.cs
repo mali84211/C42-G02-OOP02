75b75140178f264a5689bf17622ebb3344ec8e55
mali84211 <mali84211@gmail.com>
2024-07-11 23:23:10 +0300

    All Ass

---
+++ C42-G02-OOP02/Define/FileName.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static C42_G02_OOP02.Program;
+
+namespace C42_G02_OOP02.Define
+{
+    #region -1
+    internal struct Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+    #endregion
+    #region -2
+    internal struct Person1
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public Person1(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    #endregion
+
+    #region part3 -1
+    public class Employee
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int SecurityLevel { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Gender { get; set; }
+
+        public Employee(int id, string name, int securityLevel, decimal salary, DateTime hireDate, string gender)
+        {
+            ID = id;
+            Name = name;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+        }
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Security Level: {SecurityLevel}");
+            Console.WriteLine($"Salary: {Salary:C}");
+            Console.WriteLine($"Hire Date: {HireDate.ToShortDateString()}");
+            Console.WriteLine($"Gender: {Gender}");
+        }
+    }
+    #endregion
+    #region -2
+    public class HiringDate
+    {
+
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+
+        public HiringDate(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day:00}/{Month:00}/{Year}";
+        }
+    }
+
+    public class Employee1
+    {
+       
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int SecurityLevel { get; set; }
+        public decimal Salary { get; set; }
+        public HiringDate HireDate { get; set; }
+        public string Gender { get; set; }
+
+       
+        public Employee1(int id, string name, int securityLevel, decimal salary, HiringDate hireDate, string gender)
+        {
+            ID = id;
+            Name = name;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+        }
+
+        // Method to display employee details
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Security Level: {SecurityLevel}");
+            Console.WriteLine($"Salary: {Salary:C}");
+            Console.WriteLine($"Hire Date: {HireDate}");
+            Console.WriteLine($"Gender: {Gender}");
+        }
+    }
+    #endregion
+
+    #region 3
+    public class HiringDate1
+    {
+        
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+       
+        public HiringDate1(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day:00}/{Month:00}/{Year}";
+        }
+    }
+
+    public class Employee2
+    {
+        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int SecurityLevel { get; set; }
+        public decimal Salary { get; set; }
+        public HiringDate1 HireDate { get; set; }
+
+        private string gender;
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == "M" || value == "F")
+                {
+                    gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Gender must be 'M' or 'F'.");
+                }
+            }
+        }
+
+   
+        public Employee2(int id, string name, int securityLevel, decimal salary, HiringDate1 hireDate, string gender)
+        {
+            ID = id;
+            Name = name;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender; 
+        }
+
+       
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Security Level: {SecurityLevel}");
+            Console.WriteLine($"Salary: {Salary:C}");
+            Console.WriteLine($"Hire Date: {HireDate}");
+            Console.WriteLine($"Gender: {Gender}");
+        }
+
+       
+        
+    }
+
+    #endregion
+    #region 4
+    public class HiringDate2
+    {
+       
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public HiringDate2(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day:00}/{Month:00}/{Year}";
+        }
+    }
+
+    public class Employee3
+    {
+        
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public SecurityLevel SecurityLevel { get; set; }
+        public decimal Salary { get; set; }
+        public HiringDate2 HireDate { get; set; }
+
+        private string gender;
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == "M" || value == "F")
+                {
+                    gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Gender must be 'M' or 'F'.");
+                }
+            }
+        }
+
+        public Employee3(int id, string name, SecurityLevel securityLevel, decimal salary, HiringDate2 hireDate, string gender)
+        {
+            ID = id;
+            Name = name;
+            SecurityLevel = securityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender; 
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Security Level: {SecurityLevel}");
+            Console.WriteLine($"Salary: {Salary:C}");
+            Console.WriteLine($"Hire Date: {HireDate}");
+            Console.WriteLine($"Gender: {Gender}");
+        }
+    }
+    #endregion
+
+}
--- C42-G02-OOP02/Program.cs
+++ C42-G02-OOP02/Program.cs
@@ -1,10 +1,164 @@
-﻿namespace C42_G02_OOP02
+﻿using System.Text.RegularExpressions;  
+using System;
+using C42_G02_OOP02.Define;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Claims;
+
+namespace C42_G02_OOP02
 {
+
     internal class Program
     {
+        public enum SecurityLevel
+        {
+            Guest,
+            Developer,
+            Secretary,
+            DBA
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region part1
+            //AT DEMO
+            //        What You Can Write Inside The Class
+            // 1.Attributes[Fields] => Member Variable
+            // 2.Functions[Constructor, Getter Setter, Method]
+            //3.Properties[Full Property, Automatic Property, Indexer]
+            //4.Events
+            // If There is no User defined Constructor, Compiler Will Always Generate Empty Parameterless constructor => Will do Nothing
+            //If You defined a User defined Constructor ,  Compiler Will No Longer Generate Empty Parameterless constructor
+
+
+            //Class VS Struct
+
+            //Class:                                                   Struct:     
+            //Reference Type                                            Value Type                     
+            //Support 4 Pillars Of OOP                                  Support Encapsulation and Overloading in Polymorphism
+            //More Flexible                                             If You Defined a user defined Constructor ,  Compiler Will always Generate Parameterless Constructor        
+            //Does Support Inheritance                                  Doesn’t Support Inheritance           
+            //6 Access Modifier Allowed Inside It                       3 Access Modifier Allowed Inside It        
+            #endregion
+
+            #region part2 -1
+            //Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            Person[] people = new Person[3];
+            people[0] = new Person("ALi", 26);
+            people[1] = new Person("Amr", 25);
+            people[2] = new Person("Mona", 30);
+
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person);
+            }
+            #endregion
+
+            #region -2
+            // Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
+
+            Person1[] people1 = new Person1[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Enter the name of person {i + 1}: ");
+                string name = Console.ReadLine();
+
+                Console.Write($"Enter the age of person {i + 1}: ");
+                int age = int.Parse(Console.ReadLine());
+
+                people1[i] = new Person1(name, age);
+
+                Person1 oldestPerson = people1[0];
+
+                for (int j = 1; j < people1.Length; j++)
+                {
+                    if (people1[j].Age > oldestPerson.Age)
+                    {
+                        oldestPerson = people1[j];
+                    }
+                }
+                Console.WriteLine($"\nThe oldest person is {oldestPerson.Name} with an age of {oldestPerson.Age}.");
+            }
+            #endregion
+            Console.WriteLine("========================");
+            #region part3 -1
+            //Design and implement a Class for the employees in a company:
+            //Employee is identified by an ID, Name, security level, salary, hire date and Gender.
+
+            Employee emp1 = new Employee(1, "ali", 5, 9000, new DateTime(1999, 5, 1), "Male");
+            Employee emp2 = new Employee(2, "mona", 7, 7500, new DateTime(2000, 3, 15), "Female");
+            Console.WriteLine("Employee 1 Details:");
+            emp1.DisplayEmployeeInfo();
+            Console.WriteLine("\nEmployee 2 Details:");
+            emp2.DisplayEmployeeInfo();
+
+            #endregion
+            Console.WriteLine("========================");
+            #region -2
+            //Develop a Class to represent the Hiring Date Data:
+            //consisting of fields to hold the day, month and Years.
+
+            HiringDate hireDate1 = new HiringDate(1, 5, 1999);
+            Employee1 emp3 = new Employee1(1, "ali", 5, 9000, hireDate1, "Male");
+            HiringDate hireDate2 = new HiringDate(15, 3, 2000);
+            Employee1 emp4 = new Employee1(2, "mona", 7, 7500, hireDate2, "Female");
+            Console.WriteLine("Employee 3 Details:");
+            emp1.DisplayEmployeeInfo();
+            Console.WriteLine("\nEmployee 4 Details:");
+            emp2.DisplayEmployeeInfo();
+            #endregion
+            Console.WriteLine("========================");
+            #region 3
+            //We need to restrict the Gender field to be only M or F [Male or Female] 
+            try
+            {
+                
+                HiringDate1 hireDate3 = new HiringDate1(1, 5, 1999);
+                Employee2 emp5 = new Employee2(1, "ali", 5, 9000, hireDate3, "M");
+
+                HiringDate1 hireDate4 = new HiringDate1(15, 3, 2000);
+                Employee2 emp6 = new Employee2(2, "mona", 7, 7500, hireDate4, "F");
+
+                Console.WriteLine("Employee 5 Details:");
+                emp5.DisplayEmployeeInfo();
+                Console.WriteLine("\nEmployee 6 Details:");
+                emp6.DisplayEmployeeInfo();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            #endregion
+             Console.WriteLine("========================");
+            #region -4
+            try
+            {
+
+                HiringDate2 hireDate5 = new HiringDate2(1, 5, 1999);
+                Employee3 emp7 = new Employee3(1, "ali", SecurityLevel.Developer, 9000, hireDate5, "M");
+
+                HiringDate2 hireDate6 = new HiringDate2(15, 3, 2000);
+                Employee3 emp8 = new Employee3(2, "mona", SecurityLevel.Secretary, 7500, hireDate6, "F");
+
+                Console.WriteLine("Employee 7 Details:");
+                emp7.DisplayEmployeeInfo();
+                Console.WriteLine("\nEmployee 8 Details:");
+                emp8.DisplayEmployeeInfo();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            #endregion
+
         }
+
+
+
     }
+
+
+    
 }
+
